ba421f9dad51b93ad4b70d9fbaf34e5ca8c4411d
Vigneshwar <vigneshwaran2636@gmail.com>
2025-05-16 12:16:51 +0530

    Add GitHub Actions deploy to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React new
+          Learn React new other
         </a>
       </header>
     </div>
